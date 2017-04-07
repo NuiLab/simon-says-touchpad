e6d36187feab0d0acd2ece52cf20189c85f2959b
Alain Galvan <hi@alain.xyz>
2017-04-07 19:09:30 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,370 @@
+extern crate serial;
+extern crate cpal;
+
+#[macro_use]
+extern crate vulkano;
+extern crate winit;
+extern crate vulkano_win;
+
+use serial::prelude::*;
+use std::io::Read;
+
+use winit::get_primary_monitor;
+use winit::Event;
+
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use vulkano::instance::Instance;
+use vulkano::device::Device;
+use vulkano::swapchain::{Swapchain, SurfaceTransform};
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::pipeline::{GraphicsPipeline, GraphicsPipelineParams};
+use vulkano::pipeline::blend::Blend;
+use vulkano::pipeline::depth_stencil::DepthStencil;
+use vulkano::pipeline::input_assembly::InputAssembly;
+use vulkano::pipeline::vertex::SingleBufferDefinition;
+use vulkano::pipeline::multisample::Multisample;
+use vulkano::pipeline::viewport::{ViewportsState, Viewport, Scissor};
+use vulkano::framebuffer::{Framebuffer, Subpass};
+use vulkano::command_buffer;
+use vulkano::command_buffer::DynamicState;
+use vulkano::command_buffer::PrimaryCommandBufferBuilder;
+use vulkano::command_buffer::Submission;
+use vulkano_win::VkSurfaceBuild;
+
+
+mod vs {
+    include!{concat!(env!("OUT_DIR"), "/shaders/src/shaders/vert.glsl")}
+}
+
+mod fs {
+    include!{concat!(env!("OUT_DIR"), "/shaders/src/shaders/frag.glsl")}
+}
+
+fn main() {
+
+    // Port Init
+    //let mut fabric_port = serial::windows::COMPort::open("COM5").unwrap();
+    let mut buf = vec![1u8; 16];
+    //fabric_port.configure(&SETTINGS);
+    //fabric_port.set_timeout(Duration::from_secs(1));
+
+    // Vulkan Instance
+    let instance = {
+        let extensions = vulkano_win::required_extensions();
+        Instance::new(None, &extensions, None).expect("Failed to create Vulkan instance.")
+    };
+
+    // Physical Device
+    let physical = vulkano::instance::PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("No vulkan device is available.");
+
+    // OS Window
+    let window = winit::WindowBuilder::new()
+        .with_fullscreen(get_primary_monitor())
+        .build_vk_surface(&instance)
+        .unwrap();
+
+    // Graphics Queue Supported
+    let queue = physical.queue_families()
+        .find(|q| q.supports_graphics() && window.surface().is_supported(q).unwrap_or(false))
+        .expect("Couldn't find a graphical queue family.");
+
+    // Logical Device, Queues
+    let (device, mut queues) = {
+        let device_ext = vulkano::device::DeviceExtensions {
+            khr_swapchain: true,
+            ..vulkano::device::DeviceExtensions::none()
+        };
+
+        Device::new(&physical,
+                    physical.supported_features(),
+                    &device_ext,
+                    [(queue, 0.5)].iter().cloned())
+                .expect("failed to create device")
+    };
+
+    // Device Queue
+    let queue = queues.next().unwrap();
+
+    // Swapchain, Swapchain Images
+    let (swapchain, images) = {
+
+        let caps = window.surface()
+            .get_capabilities(&physical)
+            .expect("failed to get surface capabilities");
+
+        let dimensions = caps.current_extent.unwrap_or([1280, 720]);
+
+        let present = caps.present_modes.iter().next().unwrap();
+
+        let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+
+        let format = caps.supported_formats[0].0;
+
+        Swapchain::new(&device,
+                       &window.surface(),
+                       caps.min_image_count,
+                       format,
+                       dimensions,
+                       1,
+                       &caps.supported_usage_flags,
+                       &queue,
+                       SurfaceTransform::Identity,
+                       alpha,
+                       present,
+                       true,
+                       None)
+                .expect("failed to create swapchain")
+    };
+
+    // VBO and IBO
+    let vertex_buffer = {
+        #[derive(Debug, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+            uv: [f32; 2],
+        }
+        impl_vertex!(Vertex, position, uv);
+
+        CpuAccessibleBuffer::from_iter(&device,
+                                       &BufferUsage::all(),
+                                       Some(queue.family()),
+                                       [Vertex {
+                                            position: [1.0, -1.0],
+                                            uv: [1.0, 0.0],
+                                        },
+                                        Vertex {
+                                            position: [-1.0, -1.0],
+                                            uv: [0.0, 0.0],
+                                        },
+                                        Vertex {
+                                            position: [1.0, 1.0],
+                                            uv: [1.0, 1.0],
+                                        },
+                                        Vertex {
+                                            position: [-1.0, 1.0],
+                                            uv: [0.0, 1.0],
+                                        }]
+                                               .iter()
+                                               .cloned())
+                .expect("failed to create VBO")
+    };
+
+    let index_buffer = {
+        CpuAccessibleBuffer::from_iter(&device,
+                                       &BufferUsage::all(),
+                                       Some(queue.family()),
+                                       [0u32, 1, 2, 1, 2, 3]
+                                           .iter()
+                                           .cloned())
+                .expect("failed to create IBO")
+    };
+
+    // Descriptor Pool, Descriptor Set, Pipeline Layout, Uniforms
+    let descriptor_pool = vulkano::descriptor::descriptor_set::DescriptorPool::new(&device);
+
+
+
+    let uniform_buffer = {
+        CpuAccessibleBuffer::<fs::ty::Block>::from_data(&device,
+                                                        &BufferUsage::all(),
+                                                        Some(queue.family()),
+                                                        fs::ty::Block {
+                                                            mouse: [-1., -1., -1., -1.],
+                                                            resolution: [images[0].dimensions()
+                                                                             [0] as
+                                                                         f32,
+                                                                         images[0].dimensions()
+                                                                             [1] as
+                                                                         f32],
+                                                            time: 0.,
+                                                            _padding1_: 0.,
+                                                            fabric: [0., 0., 0., 0.]
+                                                        })
+                .expect("failed to create Uniform Buffer")
+    };
+
+    mod pipeline_layout {
+        pipeline_layout!{
+            set0: {
+                uniforms: UniformBuffer<::fs::ty::Block>
+            }
+        }
+    }
+
+    let pipeline_layout = pipeline_layout::CustomPipeline::new(&device).unwrap();
+
+    let set = pipeline_layout::set0::Set::new(&descriptor_pool,
+                                              &pipeline_layout,
+                                              &pipeline_layout::set0::Descriptors {
+                                                   uniforms: &uniform_buffer,
+                                               });
+
+
+    // Shaders
+    let vs = vs::Shader::load(&device).expect("failed to create shader module");
+
+    let fs = fs::Shader::load(&device).expect("failed to create shader module");
+
+    // Render Pass
+    mod render_pass {
+
+        use vulkano::format::Format;
+
+        single_pass_renderpass!{
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: Format,
+                }
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {}
+            }
+        }
+    }
+
+    let render_pass =
+        render_pass::CustomRenderPass::new(&device,
+                                           &render_pass::Formats {
+                                                // Use the format of the images and one sample.
+                                                color: (images[0].format(), 1),
+                                            })
+                .unwrap();
+
+    // Graphics Pipeline
+    let pipeline = GraphicsPipeline::new(&device,
+                                         GraphicsPipelineParams {
+                                             vertex_input: SingleBufferDefinition::new(),
+
+                                             vertex_shader: vs.main_entry_point(),
+
+                                             input_assembly: InputAssembly::triangle_list(),
+
+                                             tessellation: None,
+
+                                             geometry_shader: None,
+
+                                             viewport: ViewportsState::Fixed {
+                                                 data: vec![(Viewport {
+                                                                 origin: [0.0, 0.0],
+                                                                 depth_range: 0.0..1.0,
+                                                                 dimensions:
+                                                                     [images[0].dimensions()[0] as
+                                                                      f32,
+                                                                      images[0].dimensions()[1] as
+                                                                      f32],
+                                                             },
+                                                             Scissor::irrelevant())],
+                                             },
+
+                                             raster: Default::default(),
+
+                                             multisample: Multisample::disabled(),
+
+                                             fragment_shader: fs.main_entry_point(),
+
+                                             depth_stencil: DepthStencil::disabled(),
+
+                                             blend: Blend::pass_through(),
+
+                                             layout: &pipeline_layout,
+
+                                             render_pass: Subpass::from(&render_pass, 0).unwrap(),
+                                         })
+            .unwrap();
+
+    let framebuffers = images.iter()
+        .map(|image| {
+            let dimensions = [image.dimensions()[0], image.dimensions()[1], 1];
+            Framebuffer::new(&render_pass,
+                             dimensions,
+                             render_pass::AList { color: image })
+                    .unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    let mut submissions: Vec<Arc<Submission>> = Vec::new();
+    let mut mx: f32 = -1.0;
+    let mut my: f32 = -1.0;
+    let mut mleft: f32 = 0.0;
+    let now = Instant::now();
+
+    loop {
+
+        submissions.retain(|s| s.destroying_would_block());
+
+        let image_num = swapchain.acquire_next_image(Duration::new(1, 0)).unwrap();
+
+        let command_buffer = PrimaryCommandBufferBuilder::new(&device, queue.family())
+            .draw_inline(&render_pass,
+                         &framebuffers[image_num],
+                         render_pass::ClearValues { color: [0.0, 0.0, 0.0, 1.0] })
+            .draw_indexed(&pipeline,
+                          &vertex_buffer,
+                          &index_buffer,
+                          &DynamicState::none(),
+                          &set,
+                          &())
+            .draw_end()
+            .build();
+
+        {
+
+
+            // aquiring write lock for the uniform buffer
+            let mut buffer_content = uniform_buffer.write(Duration::new(1, 0)).unwrap();
+            // since write lock implementd Deref and DerefMut traits,
+            // we can update content directly
+            buffer_content.time = now.elapsed().as_secs() as f32 +
+                                  (now.elapsed().subsec_nanos() as f32 / 1000000000.0);
+            buffer_content.mouse = [mx, my, mleft, 0.0];
+
+
+            /*
+            buffer_content.fabric =
+                [1. - buf[3] as f32,
+                  1. - buf[7] as f32,
+                  1. - buf[11] as f32,
+                  1. - buf[15] as f32];
+                  */
+        }
+
+        submissions.push(command_buffer::submit(&command_buffer, &queue).unwrap());
+
+
+        swapchain.present(&queue, image_num).unwrap();
+
+
+        for ev in window.window().poll_events() {
+            match ev {
+                Event::KeyboardInput(winit::ElementState::Released,
+                                     _,
+                                     Some(winit::VirtualKeyCode::Escape)) => {
+                    println!("Closing visualizer!");
+                    return;
+                }
+                Event::MouseInput(winit::ElementState::Pressed, winit::MouseButton::Left) => {
+                    mleft = 1.0;
+                }
+                Event::MouseInput(winit::ElementState::Released, winit::MouseButton::Left) => {
+                    mleft = 0.0;
+                }
+                Event::MouseMoved(x, y) => {
+                    mx = x as f32;
+                    my = y as f32;
+                }
+                Event::Closed => return,
+                _ => (),
+            }
+        }
+
+        //fabric_port.read(&mut buf[..]);
+    }
+}
+
---
+++ src/shaders/frag.glsl
@@ -0,0 +1,238 @@
+#version 450
+
+/*************************************************************************
+* Uniforms
+*************************************************************************/
+
+layout(set = 0, binding = 0) uniform Block {
+    vec4 mouse;
+    vec2 resolution;
+    float time;
+    float _padding1_;
+    vec4 fabric;
+} uniforms;
+
+layout (location = 0) in vec2 iUV;
+layout (location = 0) out vec4 oColor;
+
+/*************************************************************************
+* Constants
+*************************************************************************/
+
+#define TAU 6.283185
+#define EPSILON 0.00001
+
+/*************************************************************************
+* Utilities
+*************************************************************************/
+
+//
+// Description : Array and textureless GLSL 2D/3D/4D simplex 
+//               noise functions.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+// 
+
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x) {
+     return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+float snoise(vec3 v)
+  { 
+  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+// First corner
+  vec3 i  = floor(v + dot(v, C.yyy) );
+  vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+// Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min( g.xyz, l.zxy );
+  vec3 i2 = max( g.xyz, l.zxy );
+
+  //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+  //   x1 = x0 - i1  + 1.0 * C.xxx;
+  //   x2 = x0 - i2  + 2.0 * C.xxx;
+  //   x3 = x0 - 1.0 + 3.0 * C.xxx;
+  vec3 x1 = x0 - i1 + C.xxx;
+  vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
+  vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+
+// Permutations
+  i = mod289(i); 
+  vec4 p = permute( permute( permute( 
+             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+// Gradients: 7x7 points over a square, mapped onto an octahedron.
+// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+  float n_ = 0.142857142857; // 1.0/7.0
+  vec3  ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+  vec4 x = x_ *ns.x + ns.yyyy;
+  vec4 y = y_ *ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4( x.xy, y.xy );
+  vec4 b1 = vec4( x.zw, y.zw );
+
+  //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
+  //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
+  vec4 s0 = floor(b0)*2.0 + 1.0;
+  vec4 s1 = floor(b1)*2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+  vec3 p0 = vec3(a0.xy,h.x);
+  vec3 p1 = vec3(a0.zw,h.y);
+  vec3 p2 = vec3(a1.xy,h.z);
+  vec3 p3 = vec3(a1.zw,h.w);
+
+//Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+// Mix final noise value
+  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+                                dot(p2,x2), dot(p3,x3) ) );
+  }
+
+vec3 colorDodge(vec3 inColor, vec3 blend)
+{
+    return vec3(inColor / (1.0 - blend));
+}
+
+vec3 colorOverlay(vec3 inColor, vec3 blend)
+{
+    vec3 outColor = vec3(0.);
+
+    if (inColor.r > 0.5)
+    {
+        outColor.r = (1.0 - (1.0 - 2.0 * (inColor.r - 0.5)) * (1.0 - blend.r));
+    }
+    else
+    {   
+        outColor.r = ((2.0 * inColor.r) * blend.r);
+    }
+
+    if (inColor.g > 0.5)
+    {
+        outColor.g = (1.0 - (1.0 - 2.0 * (inColor.g - 0.5)) * (1.0 - blend.g));
+    }
+    else
+    {   
+        outColor.g = ((2.0 * inColor.g) * blend.g);
+    }
+
+    if (inColor.b > 0.5)
+    {
+        outColor.b = (1.0 - (1.0 - 2.0 * (inColor.b - 0.5)) * (1.0 - blend.b));
+    }
+    else
+    {   
+        outColor.b = ((2.0 * inColor.b) * blend.b);
+    }
+
+    return outColor;
+}
+
+float saturate(float i)
+{
+    return clamp(i, 0., 1.);
+}
+
+/*************************************************************************
+* Distance Functions
+*************************************************************************/
+
+float distSin(vec2 p, float time, vec4 column)
+{
+    float f, df, d, weight, zoom;
+    zoom = 1.;
+    f = sin(p.y + time );
+    df = (f - sin(p.y + time + EPSILON )) / EPSILON;
+    weight = 1. * (
+      (1.8 - smoothstep(0.0, .98 * column[0], abs(-5.5 - p.y)))
+     * (1.8 - smoothstep(0.0, .98 * column[1], abs(-2. -  p.y)))
+     * (1.8 - smoothstep(0.0, .98 * column[2], 1. - abs(2. - p.y)))
+     * (1.8 - smoothstep(0.0, .98 * column[3], 1. - abs(5.5 - p.y)))
+    );
+
+    d = (abs(p.x * weight) - f) / sqrt(.05 + df * df);
+
+    if (p.x < 0.)
+        d = (abs(p.x * weight) + f) / sqrt(.05 + df * df);
+
+     return 1.0 - smoothstep(.0, .1, d);
+}
+
+vec3 chromaSin(vec2 p, float time, vec4 column) {
+
+    // Output
+    vec3 col = vec3(0.);
+    time *= .3;
+
+    col += vec3(distSin(p, time + (TAU / 4), column), 0., 0.);
+    col += vec3(0., distSin(vec2(1.1, 1.) * p + vec2(.1, 0.), 1.1 * -time + (2 * TAU / 4), column), 0.);
+    col += vec3(0., 0., distSin(vec2(1.2, 1.) * p + vec2(.3, 0.), 1.5 * time + (3 * TAU / 4), column));
+    return col;
+}
+/*************************************************************************
+* Main
+*************************************************************************/
+
+void main()
+{
+    // Setup
+    vec3 col = vec3(.157, .153, .169);
+    vec2 aspectRatio = vec2(1., (uniforms.resolution.y / uniforms.resolution.x));
+    vec2 uv = ((iUV - vec2(.5)) * aspectRatio) + vec2(.5);
+    vec2 uvc = (uv - vec2(.5));
+    float time = uniforms.time * 4.;
+
+    // Noise Back
+    col += mix(vec3(.157, .153, .169), abs(snoise(vec3(3. * uv, time * .1)) * vec3(.1, .12, .15)), 0.5);
+
+    // Mouse Cursor
+    vec2 mouse = (((uniforms.mouse.xy / uniforms.resolution) - vec2(.5)) * aspectRatio) + vec2(.5);
+    float cursor = (1. - saturate(dot((uv - mouse) * 16., (uv - mouse) * 16.))) * (.5 * uniforms.mouse.z);
+    col += vec3(cursor * .2);
+
+    // Vignette
+    col = mix(col, vec3(.157, .153, .169), dot(uvc * 2.5, uvc * 2.5));
+    
+    oColor = vec4(col, 1.0);
+}
