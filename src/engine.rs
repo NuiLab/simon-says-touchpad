c9cd04bd9e82bc574d2930a735c4ca05fe87ab17
Alain Galvan <hi@alain.xyz>
2017-04-20 21:17:48 -0400

    Increased threshold

--- src/engine.rs
+++ src/engine.rs
@@ -12,10 +12,10 @@ impl Game {
         let input = self.input.update();
 
         // Do some stuff...
-        let d = [if input[0] > 80. { 1. } else { 0. },
-                 if input[1] > 80. { 1. } else { 0. },
-                 if input[2] > 80. { 1. } else { 0. },
-                 if input[3] > 80. { 1. } else { 0. }];
+        let d = [if input[0] > 120. { 1. } else { 0. },
+                 if input[1] > 120. { 1. } else { 0. },
+                 if input[2] > 120. { 1. } else { 0. },
+                 if input[3] > 120. { 1. } else { 0. }];
         d
     }
 }
